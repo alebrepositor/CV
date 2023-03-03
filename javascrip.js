17692a63e7d19bbf93a964f137ca183e6360510b
alebrepositor <123869615+alebrepositor@users.noreply.github.com>
2023-03-03 18:06:20 +1000

    Update javascrip.js

--- javascrip.js
+++ javascrip.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let datos;
     $.get({
         url: 'https://randomuser.me/api/',
         dataType: 'json',
         success: function(data) {
           console.log(data);
-         
+          datos = data;
         }
     });
+    console.log(datos);
 });
